1dce0715d4a93b752a7ac0539b1a0dd186365239
IamNucassi <nucassi@bk.ru>
2023-04-16 14:23:55 +0300

    added receiving neighboring countries

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -31,7 +31,6 @@ export const Details = () => {
       </Button>
       {status === 'loading' && <h2>Loading...</h2>}
       {error && <h2>{error}</h2>}
-
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
   )
